5902a93d0876e899ae8d9d84c2f3ea2ebb5e63dc
Ayrton Ricardo da Silva <ayrton_jampa15@hotmail.com>
2013-11-07 00:48:12 -0300

    Commit inicial dos arquivos do projeto para trabalhar, a principio apenas um teste, logo podendo ser alterado.

---
+++ KitionGame/KitionGame/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace KitionGame
+{
+    partial class main_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_form));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.L1 = new System.Windows.Forms.Label();
+            this.L2 = new System.Windows.Forms.Label();
+            this.L3 = new System.Windows.Forms.Label();
+            this.L4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.A1 = new System.Windows.Forms.Label();
+            this.B1 = new System.Windows.Forms.Label();
+            this.C1 = new System.Windows.Forms.Label();
+            this.D1 = new System.Windows.Forms.Label();
+            this.A2 = new System.Windows.Forms.Label();
+            this.A3 = new System.Windows.Forms.Label();
+            this.A4 = new System.Windows.Forms.Label();
+            this.B2 = new System.Windows.Forms.Label();
+            this.B3 = new System.Windows.Forms.Label();
+            this.B4 = new System.Windows.Forms.Label();
+            this.C2 = new System.Windows.Forms.Label();
+            this.C3 = new System.Windows.Forms.Label();
+            this.C4 = new System.Windows.Forms.Label();
+            this.D2 = new System.Windows.Forms.Label();
+            this.D3 = new System.Windows.Forms.Label();
+            this.D4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // L1
+            // 
+            resources.ApplyResources(this.L1, "L1");
+            this.L1.Name = "L1";
+            // 
+            // L2
+            // 
+            resources.ApplyResources(this.L2, "L2");
+            this.L2.Name = "L2";
+            // 
+            // L3
+            // 
+            resources.ApplyResources(this.L3, "L3");
+            this.L3.Name = "L3";
+            // 
+            // L4
+            // 
+            resources.ApplyResources(this.L4, "L4");
+            this.L4.Name = "L4";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // A1
+            // 
+            this.A1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.A1, "A1");
+            this.A1.Name = "A1";
+            this.A1.Tag = "ayrton";
+            // 
+            // B1
+            // 
+            this.B1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.B1, "B1");
+            this.B1.Name = "B1";
+            // 
+            // C1
+            // 
+            this.C1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.C1, "C1");
+            this.C1.Name = "C1";
+            // 
+            // D1
+            // 
+            this.D1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.D1, "D1");
+            this.D1.Name = "D1";
+            // 
+            // A2
+            // 
+            this.A2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.A2, "A2");
+            this.A2.Name = "A2";
+            // 
+            // A3
+            // 
+            this.A3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.A3, "A3");
+            this.A3.Name = "A3";
+            // 
+            // A4
+            // 
+            this.A4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.A4, "A4");
+            this.A4.Name = "A4";
+            // 
+            // B2
+            // 
+            this.B2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.B2, "B2");
+            this.B2.Name = "B2";
+            // 
+            // B3
+            // 
+            this.B3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.B3, "B3");
+            this.B3.Name = "B3";
+            // 
+            // B4
+            // 
+            this.B4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.B4, "B4");
+            this.B4.Name = "B4";
+            // 
+            // C2
+            // 
+            this.C2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.C2, "C2");
+            this.C2.Name = "C2";
+            // 
+            // C3
+            // 
+            this.C3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.C3, "C3");
+            this.C3.Name = "C3";
+            // 
+            // C4
+            // 
+            this.C4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.C4, "C4");
+            this.C4.Name = "C4";
+            // 
+            // D2
+            // 
+            this.D2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.D2, "D2");
+            this.D2.Name = "D2";
+            // 
+            // D3
+            // 
+            this.D3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.D3, "D3");
+            this.D3.Name = "D3";
+            // 
+            // D4
+            // 
+            this.D4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.D4, "D4");
+            this.D4.Name = "D4";
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // main_form
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.D4);
+            this.Controls.Add(this.D3);
+            this.Controls.Add(this.D2);
+            this.Controls.Add(this.D1);
+            this.Controls.Add(this.C4);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B4);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A4);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.L4);
+            this.Controls.Add(this.L3);
+            this.Controls.Add(this.L2);
+            this.Controls.Add(this.L1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Silver;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main_form";
+            this.TransparencyKey = System.Drawing.Color.DarkRed;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label L1;
+        private System.Windows.Forms.Label L2;
+        private System.Windows.Forms.Label L3;
+        private System.Windows.Forms.Label L4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label A1;
+        private System.Windows.Forms.Label B1;
+        private System.Windows.Forms.Label C1;
+        private System.Windows.Forms.Label D1;
+        private System.Windows.Forms.Label A2;
+        private System.Windows.Forms.Label A3;
+        private System.Windows.Forms.Label A4;
+        private System.Windows.Forms.Label B2;
+        private System.Windows.Forms.Label B3;
+        private System.Windows.Forms.Label B4;
+        private System.Windows.Forms.Label C2;
+        private System.Windows.Forms.Label C3;
+        private System.Windows.Forms.Label C4;
+        private System.Windows.Forms.Label D2;
+        private System.Windows.Forms.Label D3;
+        private System.Windows.Forms.Label D4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ KitionGame/KitionGame/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KitionGame
+{
+    public partial class main_form : Form
+    {
+        public main_form()
+        {
+            InitializeComponent();
+            Label barco1 = new Label();
+            barco1.Text = "lol";
+            barco1.Text = A1.Tag.ToString();
+            barco1.Tag = A1;
+            Label teste = new Label();
+            teste = (Label)barco1.Tag;
+            richTextBox1.Text = teste.Text; 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+    }
+}
